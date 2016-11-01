11f2a033c004a89097ed3610f798ff56c34d7ca2
Matt Raible <matt@raibledesigns.com>
2016-11-01 08:25:50 -0600

    Code formatting and bug fixes

--- src/main/java/tutorial/DatabaseLoader.java
+++ src/main/java/tutorial/DatabaseLoader.java
@@ -1,24 +1,24 @@
 package tutorial;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-	private final EmployeeRepository repository;
+    private final EmployeeRepository repository;
 
-	@Autowired
-	public DatabaseLoader(EmployeeRepository repository) {
-		this.repository = repository;
-	}
+    @Autowired
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
-		this.repository.save(new Employee("Joe Biden", 45, 5));
-		this.repository.save(new Employee("President Obama", 54, 8));
-		this.repository.save(new Employee("Crystal Mac", 34, 12));
-		this.repository.save(new Employee("James Henry", 33, 2));
-	}
+    @Override
+    public void run(String... strings) throws Exception {
+        this.repository.save(new Employee("Joe Biden", 45, 5));
+        this.repository.save(new Employee("President Obama", 54, 8));
+        this.repository.save(new Employee("Crystal Mac", 34, 12));
+        this.repository.save(new Employee("James Henry", 33, 2));
+    }
 }
--- src/main/java/tutorial/EmployeeRepository.java
+++ src/main/java/tutorial/EmployeeRepository.java
@@ -1,11 +1,12 @@
 package tutorial;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
-	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
-  	@Override
-  	void delete(Long aLong);
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @Override
+    void delete(Long aLong);
 
 }
